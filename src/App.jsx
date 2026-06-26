f48ea9498297d672198951dece7f8233b8a4de02
Mahliyo <gasandigital13@gmail.com>
2026-06-26 19:12:10 +0500

    Tailwind star

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function App() {
   return (
     <div className="cards">
-      <h1 className="text-red"></h1>
+      <h1 className="text-red-600 w-1" >Welcom Tailwind</h1>
     </div>
   )
 }
