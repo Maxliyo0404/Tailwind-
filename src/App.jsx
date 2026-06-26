c5bcf0a1c32b1b5d1d8f6792bce0aa08fde7ce8a
Mahliyo <gasandigital13@gmail.com>
2026-06-26 23:45:49 +0500

    Tailwind star text

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="bg-black mx- w-17" >
+    <div className="bg-black mx- w-1200" >
       <h1 className="text-white w-1" >Welcom Tailwind</h1>
       <ul className="list">
         <li className="item">
