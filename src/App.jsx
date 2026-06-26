3e938fd698403c5b5f10f94306101625537880cf
Mahliyo <gasandigital13@gmail.com>
2026-06-26 23:53:35 +0500

    Tailwind star responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 
 function App() {
   return (
-    // mx-auto va max-w-7xl markazlash uchun ishlatiladi
-    // bg-black to'g'ri, lekin ranglar uchun text-white alohida bo'lishi kerak
+    
     <div className="bg-black mx-auto max-w-[1200px] p-5"> 
       
       <h1 className="text-4xl text-white">Welcome Tailwind</h1>
