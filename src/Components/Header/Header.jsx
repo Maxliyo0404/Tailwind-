150c1bd0d3217ff448cb0a062291028e5693f384
Mahliyo <gasandigital13@gmail.com>
2026-06-27 23:15:04 +0500

    header Tailwinddafnish

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <div className="container flex items-center justify-between">
             <div className="flex items-center justify-between">
                 <h1 className="title">Logo</h1>
-                <ul className="list flex items-center ">
+                <ul className="list flex items-center gap-5">
                     <li className="text-white"><a href="#">Home</a></li>
                     <li className="text-white"><a href="#">About</a></li>
                     <li className="text-white"><a href="#">Contact</a></li>
