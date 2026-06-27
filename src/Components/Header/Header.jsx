0c76c11832d96f3163931843ef663b7c81397277
Mahliyo <gasandigital13@gmail.com>
2026-06-27 22:43:29 +0500

    Tailwind css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
      <div className="cards">
         <div className="container">
             <div className="card">
-                <h1>Logo</h1>
+                <h1 className="title">Logo</h1>
             </div>
         </div>
      </div>
