e87448f8e0fb8641117166d807593703e6a12016
Mahliyo <gasandigital13@gmail.com>
2026-06-27 23:14:40 +0500

    header Tailwindda

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,11 +7,11 @@ function Header() {
         <div className="container flex items-center justify-between">
             <div className="flex items-center justify-between">
                 <h1 className="title">Logo</h1>
-                <ul className="list">
-                    <li className="item"><a href="#">Home</a></li>
-                    <li className="item"><a href="#">About</a></li>
-                    <li className="item"><a href="#">Contact</a></li>
-                    <li className="item"><a href="#">Services</a></li>
+                <ul className="list flex items-center ">
+                    <li className="text-white"><a href="#">Home</a></li>
+                    <li className="text-white"><a href="#">About</a></li>
+                    <li className="text-white"><a href="#">Contact</a></li>
+                    <li className="text-white"><a href="#">Services</a></li>
                 </ul>
                 <button className="border-black 2xl:">yuborish</button>
             </div>
