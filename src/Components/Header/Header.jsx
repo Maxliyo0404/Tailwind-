8c182ecea8664bac8cbab5006e596cb38583941f
Mahliyo <gasandigital13@gmail.com>
2026-06-27 22:51:58 +0500

    Tailwind css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1 className="title">Logo</h1>
                 <ul className="list">
                     <li className="item"><a href="#">Home</a></li>
-                    <li className="item"><a href="#"></a></li>
+                    <li className="item"><a href="#">About</a></li>
                     <li className="item"><a href="#"></a></li>
                     <li className="item"><a href="#"></a></li>
                 </ul>
