cef7be2d3e8cc0242224e10c90fa6c9b7b6e00d7
Mahliyo <gasandigital13@gmail.com>
2026-06-27 22:37:46 +0500

    Tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import React from 'react'
 function App() {
   return (
     
-    <div className="bg-black mx-auto max-w-[1200px] p-5 rounded-full " > 
+  //  <div className="bg-black mx-auto max-w-[1200px] p-5 rounded-full " > 
       
-      <h1 className="text-8xl text-white">Welcome Tailwind</h1>
+  //     <h1 className="t ext-8xl text-white">Welcome Tailwind</h1>
       
-      <ul className="list-disc text-white">
-        <li className="item">
+  //     <ul className="list-disc text-white">
+  //       <li className="item">
        
-          <h2 className="text-4xl text-white">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit...
-          </h2>
-          <p className="text-2xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas mollitia voluptates ipsam corrupti doloribus autem, necessitatibus officia optio esse ipsum doloremque vitae ad, voluptatum commodi, illum cum aspernatur eius pariatur.</p>
-        </li>
-      </ul>
-    </div>
+  //         <h2 className="text-4xl text-white">
+  //           Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+  //         </h2>
+  //         <p className="text-2xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas mollitia voluptates ipsam corrupti doloribus autem, necessitatibus officia optio esse ipsum doloremque vitae ad, voluptatum commodi, illum cum aspernatur eius pariatur.</p>
+  //       </li>
+  //     </ul>
+  //   </div>
   )
 }
 
