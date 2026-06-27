d4cf913337f1e4edd1bcd48568c7e56818326483
Mahliyo <gasandigital13@gmail.com>
2026-06-27 23:16:39 +0500

    header Tailwindda fnish

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 function Header() {
   return (
     <header className="bg-yellow-400 py-5">
-        <div className="container flex items-center justify-between">
+        <div className="container ">
             <div className="flex items-center justify-between">
                 <h1 className="title">Logo</h1>
                 <ul className="list flex items-center gap-5">
-                    <li className="text-white"><a href="#">Home</a></li>
-                    <li className="text-white"><a href="#">About</a></li>
-                    <li className="text-white"><a href="#">Contact</a></li>
-                    <li className="text-white"><a href="#">Services</a></li>
+                    <li className="text-white "><a href="#">Home</a></li>
+                    <li className="text-white "><a href="#">About</a></li>
+                    <li className="text-white "><a href="#">Contact</a></li>
+                    <li className="text-white "><a href="#">Services</a></li>
                 </ul>
                 <button className="border-black 2xl:">yuborish</button>
             </div>
