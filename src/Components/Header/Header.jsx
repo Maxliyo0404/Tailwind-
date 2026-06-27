06d8c13faeca57aea05d0b58441e44be88fd23c5
Mahliyo <gasandigital13@gmail.com>
2026-06-27 23:04:50 +0500

    header Tailwindda

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Header() {
   return (
     <header className="bg-green-950 py-5">
-        <div className="container flex items-center ">
+        <div className="container flex items-center justify-between">
             <div className="">
                 <h1 className="title">Logo</h1>
                 <ul className="list">
