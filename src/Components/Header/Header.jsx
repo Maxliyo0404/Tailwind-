dec40b7b620a17f223e4f4e6ccab158d15941d65
Mahliyo <gasandigital13@gmail.com>
2026-06-28 11:42:35 +0500

    DarkModule

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 function Header() {
     const [darkMode, setDarkMode] = useState(false);
   return (
-    <header className="bg-yellow-400 py-5">
+    <header className="bg-black py-5">
         <div className="container ">
             <div className="flex items-center justify-between">
-                <h1 className="title">Logo</h1>
+                <h1 className="text-white ">Logo</h1>
                 <ul className="list flex items-center gap-5">
                     <li className="text-white "><a href="#">Home</a></li>
                     <li className="text-white "><a href="#">About</a></li>
