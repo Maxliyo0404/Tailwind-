33cd99e84f118928b5a731f160bafaa39676046d
Mahliyo <gasandigital13@gmail.com>
2026-06-28 14:35:25 +0500

    DarkModule css darMode

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ return(
     <header className="bg-blue-900 py-5">
         <div className="container flex items-center justify-between ">
             <h1>Logo</h1>
-            <ul>
+            <ul className="flex gap-6">
                 <li><a className="text-2xl text-white" href="#">Home</a></li>
                 <li><a className="text-2xl text-white" href="#">About</a></li>
                 <li><a className="text-2xl text-white" href="#">Contact</a></li>
