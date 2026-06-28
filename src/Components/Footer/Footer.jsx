e7920ac0719d208deff8b9b2ab7788c015bca639
Mahliyo <gasandigital13@gmail.com>
2026-06-28 18:21:05 +0500

    fnish

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-blue-800 dark:bg-gray-900 text-white py-8 transition-colors duration-300">
       <div className="container text-center">
-        <p className="text-lg">© 2026 Mening Saytim. Barcha huquqlar himoyalangan.</p>
+        <p className="text-lg">© 2026 Mening Saytim. Barcha huquqlar himoyalangan..</p>
       </div>
     </footer>
   );
