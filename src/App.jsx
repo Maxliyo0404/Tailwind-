c834a46385376f5af07a63e8f3d35a909908d3d6
Mahliyo <gasandigital13@gmail.com>
2026-06-28 15:09:35 +0500

    pages star

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import React from 'react'
 import Layout from "./Components/Layout/Layout";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 function App() {
   return (
     <>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<Layout/>}>
+   <Route path="home" element={<Home/>}/>
+   <Route path="about" element={<About/>}/>
+  </Route>
+  </Routes>
+  </BrowserRouter>
+   
    
     </>
   )
