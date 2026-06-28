a4f0f986347d55b11f90cb99fe584fb55ec6f59f
Mahliyo <gasandigital13@gmail.com>
2026-06-28 14:44:38 +0500

    DarkModule css darMode responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,42 +3,6 @@ import "./Header.css";
 import React from 'react'
 
 function Header() {
-    const [darkMode, setDarkMode] = useState(false);
-    useEffect(()=>{
-        if(darkMode){
-            document.body.classList.add("dark");
-            document.body.classList.remove("light");
-        } else{
-            document.body.classList.add("light");
-            document.body.classList.remove("dark");
-        }
-    }, [darkMode])
-return(
-    <>
-    <header className="bg-blue-900 py-5">
-        <div className="container flex items-center justify-between ">
-            <h1 className="text-4xl text-white font-bold">Logo</h1>
-            <ul className="flex gap-5">
-                <li><a className="text-2xl text-white" href="#">Home</a></li>
-                <li><a className="text-2xl text-white" href="#">About</a></li>
-                <li><a className="text-2xl text-white" href="#">Contact</a></li>
-                <li><a className="text-2xl text-white" href="#">Services</a></li>
-            </ul>
-            <button onClick={()=>setDarkMode(!darkMode)}>
-             {darkMode ? "🌙" : "☀️"}
-            </button>
-        </div>
-    </header>
-    
-    </>
-)
-
-
-
-
-
-
-
 //     const [darkMode, setDarkMode] = useState(false);
 //     useEffect (()=>{
 //         if (darkMode){
