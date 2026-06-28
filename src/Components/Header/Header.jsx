ec7c168e45a2f8290c55b6e45fd449e12dee3d81
Mahliyo <gasandigital13@gmail.com>
2026-06-28 14:26:48 +0500

    DarkModule css darMode

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,35 +3,43 @@ import "./Header.css";
 import React from 'react'
 
 function Header() {
-    const [darkMode, setDarkMode] = useState(false);
-    useEffect (()=>{
-        if (darkMode){
-            document.body.classList.add("dark");
-            document.body.classList.remove("light");
-        }else{
-            document.body.classList.add("light");
-            document.body.classList.remove("dark");
-        }  
-    }, [darkMode])
-  return (
-    <header className="bg-black py-5">
-        <div className="container ">
-            <div className="flex items-center justify-between">
-                <h1 className="text-4xl text-white font-bold">Logo</h1>
-                <ul className="list flex items-center gap-5">
-                    <li className="text-white "><a href="#">Home</a></li>
-                    <li className="text-white "><a href="#">About</a></li>
-                    <li className="text-white "><a href="#">Contact</a></li>
-                    <li className="text-white "><a href="#">Services</a></li>
-                </ul>
-                <button className="text-3xl" onClick={()=>setDarkMode(!darkMode)}>
-                    {darkMode ? "🌙" : "☀️"}
-                </button>
-            </div>
-        </div>
+
+
+
+
+
+
+
+    
+//     const [darkMode, setDarkMode] = useState(false);
+//     useEffect (()=>{
+//         if (darkMode){
+//             document.body.classList.add("dark");
+//             document.body.classList.remove("light");
+//         }else{
+//             document.body.classList.add("light");
+//             document.body.classList.remove("dark");
+//         }  
+//     }, [darkMode])
+//   return (
+//     <header className="bg-black py-5">
+//         <div className="container ">
+//             <div className="flex items-center justify-between">
+//                 <h1 className="text-4xl text-white font-bold">Logo</h1>
+//                 <ul className="list flex items-center gap-5">
+//                     <li className="text-white "><a href="#">Home</a></li>
+//                     <li className="text-white "><a href="#">About</a></li>
+//                     <li className="text-white "><a href="#">Contact</a></li>
+//                     <li className="text-white "><a href="#">Services</a></li>
+//                 </ul>
+//                 <button className="text-3xl" onClick={()=>setDarkMode(!darkMode)}>
+//                     {darkMode ? "🌙" : "☀️"}
+//                 </button>
+//             </div>
+//         </div>
      
-    </header>
-  )
+//     </header>
+//   )
 }
 
 export default Header
